7a1e08c46134192878aa7d93b08a4321793a04b8
Drew Covi <drew.covi@honeywell.com>
2018-08-03 14:53:27 -0500

    adding wifi-connect command back to start script

--- app/start.sh
+++ app/start.sh
@@ -38,13 +38,13 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 # wget --spider http://google.com 2>&1
 
 # 4. Is there an active WiFi connection?
-# iwgetid -r
+iwgetid -r
 
-# if [ $? -eq 0 ]; then
-#     printf 'Skipping WiFi Connect\n'
-# else
-#     printf 'Starting WiFi Connect\n'
-#     ./wifi-connect
-# fi
+if [ $? -eq 0 ]; then
+    printf 'Skipping WiFi Connect\n'
+else
+    printf 'Starting WiFi Connect\n'
+    ./wifi-connect
+fi
 
 # Start your application here.
