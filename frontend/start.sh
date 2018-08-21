5ec0e0de638582b66a39540f8bd8346438c06595
Drew Covi <drew.covi@honeywell.com>
2018-08-21 10:58:44 -0500

    updated layout. tiemzone support added

--- frontend/start.sh
+++ frontend/start.sh
@@ -3,6 +3,20 @@
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 
+#!/usr/bin/bash
+
+# Default to UTC if no TIMEZONE env variable is set
+echo "Setting time zone to ${TIMEZONE=UTC}"
+# This only works on Debian-based images
+echo "${TIMEZONE}" > /etc/timezone
+dpkg-reconfigure tzdata
+
+# Replace this below with your own application start
+# It just idles in this example.
+while : ; do
+    date
+    sleep 60
+done
 
 # Start your application here.
 
